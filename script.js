7e7e085fe3d079bed662f7a10c147e6079a0d028
lismaxserg <hardy45@mail.ru>
2019-09-06 22:26:08 +0300

    working without proto

--- script.js
+++ script.js
@@ -1,12 +1,69 @@
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
+
 const fsread = require('fs');
 
 fsread.readFile('input.txt', 'utf8', function(error, data) {
   if (error) throw error; // если возникла ошибка
-  let arr = data.split(/\s/);
+
+const dictionary = {
+    1: '.----',
+    2: '..---',
+    3: '...--',
+    4: '....-',
+    5: '.....',
+    6: '-....',
+    7: '--...',
+    8: '---..',
+    9: '----.',
+    0: '-----'
+}
+
+    const arr = data.split("   ");
+    let result  = '';
+
+    for (const iterator of arr) {
+        if(result != ''){
+            result  = result + ' ';
+        }
+        let toDecode = iterator.split(' ');
+        for (let iteratorDecode of toDecode) {
+            if(iteratorDecode[0] == 'E'){
+                iteratorDecode = iteratorDecode.substr(1);
+                let o = iteratorDecode[0];
+                let newstr = iteratorDecode.substr(0, 0) + iteratorDecode[iteratorDecode.length-1]+ iteratorDecode.substr(0 + iteratorDecode[iteratorDecode.length-1].length);
+                let finalstr = newstr.substr(0, iteratorDecode.length-1) + o+ newstr.substr(iteratorDecode.length-1 + o.length)
+                result = result + decodeFromMorzeToNumbers(finalstr);
+            } else if (iteratorDecode[0] == 'R') {
+                iteratorDecode = iteratorDecode.substr(1);
+                let finalstr =''
+                for (let index = 0; index < iteratorDecode.length; index++) {
+                    if(index % 2 == 0){
+                        finalstr = finalstr + iteratorDecode[index];
+                    }
+                    
+                }
+                result = result + decodeFromMorzeToNumbers(finalstr);
+
+            } else {
+                result = result + decodeFromMorzeToNumbers(iteratorDecode);
+            }
+
+        }
+    }
+
+    function decodeFromMorzeToNumbers(text){
+        for (key in dictionary) {
+            if(dictionary[key] == text){
+                return key;
+            }
+        }
+    }
 
   const fswrite = require('fs');
 
-  fswrite.writeFile('output.txt', arr.toString(), (err) => {
+  fswrite.writeFile('output.txt', result, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
